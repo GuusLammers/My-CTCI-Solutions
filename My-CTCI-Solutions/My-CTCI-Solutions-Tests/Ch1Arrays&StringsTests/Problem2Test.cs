6f7045baf7ce4b2a57e2fa27499b9f2be1b2dad3
GuusLammers <guuslammers16@gmail.com>
2022-06-30 16:29:24 -0700

    Finished Ch.1 problem 2.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem2Test.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem2Test
+    {
+        [TestMethod]
+        public void ArePermutationsTest1True()
+        {
+            // arrange
+            String str1 = "hello";
+            String str2 = "ollhe";
+            bool expected = true;
+
+            // act
+            bool actual = Problem2.ArePermutations(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ArePermutationsTest2False()
+        {
+            // arrange
+            String str1 = "hello";
+            String str2 = "hell";
+            bool expected = false;
+
+            // act
+            bool actual = Problem2.ArePermutations(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ArePermutationsTest3EmptyStrings()
+        {
+            // arrange
+            String str1 = "";
+            String str2 = "";
+            bool expected = true;
+
+            // act
+            bool actual = Problem2.ArePermutations(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem2
+    {
+        public static bool ArePermutations(String str1, String str2)
+        {
+            char[] str1CharArray = str1.ToCharArray();
+            char[] str2CharArray = str2.ToCharArray();
+
+            Array.Sort(str1CharArray);
+            Array.Sort(str2CharArray);
+
+            if(str1CharArray.SequenceEqual(str2CharArray))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
