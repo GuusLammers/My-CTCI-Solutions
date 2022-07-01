e4a0a26610eef94c75d033a052f64d1d8870321c
GuusLammers <guuslammers16@gmail.com>
2022-07-01 14:28:53 -0700

    Finished Ch.1 problem 4.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem4Test.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem4Test
+    {
+        [TestMethod]
+        public void IsPalindromeTest1True()
+        {
+            // arrange
+            String str = "racecar";
+            bool expected = true;
+
+            // act
+            bool actual = Problem4.IsPalindrome(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest2False()
+        {
+            // arrange
+            String str = "raceca";
+            bool expected = false;
+
+            // act
+            bool actual = Problem4.IsPalindrome(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest3Empty()
+        {
+            // arrange
+            String str = "";
+            bool expected = true;
+
+            // act
+            bool actual = Problem4.IsPalindrome(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PalindromePermutationsTest1True()
+        {
+            // arrange
+            String str = "Tact Coa";
+            bool expected = true;
+
+            // act
+            bool actual = Problem4.PalindromePermutation(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PalindromePermutationsTest2False()
+        {
+            // arrange
+            String str = "Tact Co";
+            bool expected = false;
+
+            // act
+            bool actual = Problem4.PalindromePermutation(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PalindromePermutationsTest3Empty()
+        {
+            // arrange
+            String str = "";
+            bool expected = true;
+
+            // act
+            bool actual = Problem4.PalindromePermutation(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem4
+    {
+        // Input: Tact Coa
+        // Output: True(permutations: "taco cat", "atco cta", etc.)
+
+        public static bool PalindromePermutation(String str)
+        {
+            Dictionary<char, int> dict = new();
+
+            int numberOfCharactersWithOddCount = 0;
+            
+            foreach(char c in str.ToLower())
+            {
+                if(Char.IsWhiteSpace(c))
+                {
+                    continue;
+                }
+
+                if(!dict.ContainsKey(c))
+                {
+                    dict.Add(c, 1);
+                    numberOfCharactersWithOddCount++;
+                }
+                else
+                {
+                    dict[c]++;
+
+                    if(dict[c] % 2 == 0)
+                    {
+                        numberOfCharactersWithOddCount--;
+                    }
+                    else
+                    {
+                        numberOfCharactersWithOddCount++;
+                    }
+                }
+            }
+
+            if(numberOfCharactersWithOddCount <= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if the passed in string is a palindrome.
+        /// </summary>
+        /// <param name="str">Contains no spaces.</param>
+        /// <returns>True if the string is a palindrome.</returns>
+        public static bool IsPalindrome(String str)
+        {
+            char[] charArray = str.ToCharArray();
+            char[] charArrayReversed = str.ToCharArray();
+            Array.Reverse(charArrayReversed);
+
+            if (charArray.SequenceEqual(charArrayReversed))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
