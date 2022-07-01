cfa5cb65d2ca0b1e1e0b8ccc269d55eb45db78ee
GuusLammers <guuslammers16@gmail.com>
2022-07-01 14:28:33 -0700

    Finished Ch.1 problem 3.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem3Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem3Test
+    {
+        [TestMethod]
+        public void URLifyTest1()
+        {
+            // arrange
+            String str = "Mr John Smith    ";
+            int length = 13;
+            String expected = "Mr%20John%20Smith";
+
+            // act
+            String actual = Problem3.URLify(str, length);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void URLifyTest2EmptyString()
+        {
+            // arrange
+            String str = "";
+            int length = 10;
+            String expected = "";
+
+            // act
+            String actual = Problem3.URLify(str, length);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem3.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem3
+    {
+        /// Input: "Mr John Smith    ", 13
+        /// Output: "Mr%20John%20Smith"
+        public static String URLify(String str, int length)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (i < length)
+                {
+                    if (Char.IsWhiteSpace(str, i))
+                    {
+                        strBuilder.Append("%20");
+                        continue;
+                    }
+
+                    strBuilder.Append(str.ElementAt(i));
+                }
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
+
