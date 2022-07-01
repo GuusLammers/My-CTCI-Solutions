3c3973e7e42d3272c552773ba78b5a692578084f
GuusLammers <guuslammers16@gmail.com>
2022-07-01 15:38:10 -0700

    Finished Ch.1 problem 6.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem6Test.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem6Test
+    {
+        [TestMethod]
+        public void StringCompressionTest1Compressed()
+        {
+            // arrange
+            String str = "aabcccccaaa";
+            String expected = "a2b1c5a3";
+
+            // act
+            String actual = Problem6.StringCompression(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StringCompressionTest2NotCompressed()
+        {
+            // arrange
+            String str = "abca";
+            String expected = "abca";
+
+            // act
+            String actual = Problem6.StringCompression(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StringCompressionTest3Empty()
+        {
+            // arrange
+            String str = "";
+            String expected = "";
+
+            // act
+            String actual = Problem6.StringCompression(str);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem6.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem6
+    {
+        // For example, the string aabcccccaaa would become a2blc5a3.If the
+        // "compressed" string would not become smaller than the original string, your method should return
+        // the original string. You can assume the string has only uppercase and lowercase letters(a - z).
+        public static String StringCompression(String str)
+        {
+            if(str.Length.Equals(0))
+            {
+                return str;
+            }
+
+            StringBuilder stringBuilder = new();
+
+            char currentChar = str.ElementAt(0);
+            int charCount = 1;
+            for(int i = 1; i < str.Length; i++)
+            {
+                if(str.ElementAt(i).Equals(currentChar))
+                {
+                    charCount++;
+                }
+                else if(!str.ElementAt(i).Equals(currentChar)) 
+                {
+                    stringBuilder.Append(currentChar);
+                    stringBuilder.Append(charCount);
+                    currentChar = str.ElementAt(i);
+                    charCount = 1;
+                }
+            }
+
+            stringBuilder.Append(currentChar);
+            stringBuilder.Append(charCount);
+
+            if (stringBuilder.Length < str.Length)
+            {
+                return stringBuilder.ToString();
+            }
+            else
+            {
+                return str;
+            }
+            
+        }
+    }
+}
+
