1ec6b3704963dd0c0fe5c9b84a0d21dd323b4301
GuusLammers <guuslammers16@gmail.com>
2022-07-01 15:06:21 -0700

    Finished Ch1. problem 5.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem5Test.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem5Test
+    {
+        [TestMethod]
+        public void IsPalindromeTest1TrueRemove()
+        {
+            // arrange
+            String str1 = "pale";
+            String str2 = "ple";
+            bool expected = true;
+
+            // act
+            bool actual = Problem5.OneEdit(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest2TrueInsert()
+        {
+            // arrange
+            String str1 = "ple";
+            String str2 = "pale";
+            bool expected = true;
+
+            // act
+            bool actual = Problem5.OneEdit(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest3TrueReplace()
+        {
+            // arrange
+            String str1 = "pale";
+            String str2 = "bale";
+            bool expected = true;
+
+            // act
+            bool actual = Problem5.OneEdit(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest4FalseDifferingLength()
+        {
+            // arrange
+            String str1 = "pales";
+            String str2 = "ple";
+            bool expected = false;
+
+            // act
+            bool actual = Problem5.OneEdit(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTest5FalseIdentical()
+        {
+            // arrange
+            String str1 = "pale";
+            String str2 = "pale";
+            bool expected = false;
+
+            // act
+            bool actual = Problem5.OneEdit(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem5.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem5
+    {
+        // There are three types of edits that can be performed on strings: insert a character,
+        // remove a character, or replace a character.Given two strings, write a function to check if they are
+        // one edit (or zero edits) away.
+        // EXAMPLE
+        // pale, ple -> true
+        // pales, pale -> true
+        // pale, bale -> true
+        // pale, bake -> false
+        public static bool OneEdit(String str1, String str2)
+        {
+            // length of strings differs by more then one character
+            if(Math.Abs(str1.Length - str2.Length) > 1)
+            {
+                return false;
+            }
+
+            int str1Pointer = 0, str2Pointer = 0, edits = 0;
+            while(str1Pointer < str1.Length && str2Pointer < str2.Length)
+            {
+                // characters at str1Pointer and str2Pointer are the same
+                if(str1.ElementAt(str1Pointer).Equals(str2.ElementAt(str2Pointer)))
+                {
+                    str1Pointer++;
+                    str2Pointer++;
+                    continue;
+                }
+
+                // an edit is required if this point is reached
+                edits++;
+                if(edits > 1)
+                {
+                    return false;
+                }
+
+                // check if an replace edit is required
+                if (str1.Length == str2.Length)
+                {
+                    str1Pointer++;
+                    str2Pointer++;
+                }
+                // check if an insert edit is required
+                else if(str1.Length - str2.Length < 0)
+                {
+                    str2Pointer++;
+                }
+                // remove edit is required
+                else
+                {
+                    str1Pointer++;
+                }
+            }
+
+            return edits == 1;
+        }
+    }
+}
+
