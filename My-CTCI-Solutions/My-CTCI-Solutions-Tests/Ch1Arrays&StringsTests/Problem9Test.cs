b3c3ee91d69d72886ccb6f25f89d876198a16617
GuusLammers <guuslammers16@gmail.com>
2022-07-03 10:11:50 -0700

    Finished Ch.1 Problem 9.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem9Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem9Test
+    {
+        [TestMethod]
+        public void IsRotationTest1True()
+        {
+            // arrange
+            String str1 = "waterbottle";
+            String str2 = "erbottlewat";
+            bool expected = true;
+
+            // act
+            bool actual = Problem9.StringRotation(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsRotationTest2False()
+        {
+            // arrange
+            String str1 = "waterbottle";
+            String str2 = "ttewaterbot";
+            bool expected = false;
+
+            // act
+            bool actual = Problem9.StringRotation(str1, str2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem9
+    {
+        // Rotation:Assume you have a method isSubstring which checks if oneword is a substring
+        // of another.Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
+        // call to isSubstring(e.g., "waterbottle" is a rotation of"erbottlewat").
+
+        public static bool StringRotation(String str1, String str2)
+        {
+            if(str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            return IsSubstring(str1 + str1, str2);
+        }
+
+        public static bool IsSubstring(String str1str1, String str2)
+        {
+            return str1str1.Contains(str2);
+        }
+    }
+}
+
