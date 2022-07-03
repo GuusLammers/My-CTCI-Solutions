15fe4050965b21ffb315d170a340894bca634b87
GuusLammers <guuslammers16@gmail.com>
2022-07-03 15:31:21 -0700

    Adding Node class for Ch.2 LInked LIsts questions.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch2LinkedLists/Node.cs
@@ -0,0 +1,41 @@
+﻿namespace My_CTCI_Solutions.Ch2LinkedLists
+{
+    public class Node
+    {
+        Node? next = null;
+        int value;
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+
+        public void AppendToTail(int value)
+        {
+            Node newNode = new(value);
+            Node currentNode = this;
+            while(currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = newNode;
+        }
+
+        public Node GetNextNode()
+        {
+            return next;
+        }
+
+        public void SetNextNode(Node node)
+        {
+            this.next = node;
+        }
+
+        public int GetValue()
+        {
+            return this.value;
+        }
+    }
+}
+
