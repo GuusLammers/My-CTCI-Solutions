77d13e58fcb8e59de476deca4ccd3d202bf363a6
GuusLammers <guuslammers16@gmail.com>
2022-07-03 10:12:48 -0700

    Commiting temporary files.

--- My-CTCI-Solutions/My-CTCI-Solutions/MainClass.cs
+++ My-CTCI-Solutions/My-CTCI-Solutions/MainClass.cs
@@ -6,9 +6,26 @@ namespace My_CTCI_Solutions
     {
         static void Main(string[] args)
         {
-            String str = "abcdefghijklmnopqrstuvwxyza";
-            Problem1.IsUniqueInPlace(str);
+            int[,] image = new int[4, 5] {
+                {1, 2, 3, 4, 5},
+                {5, 0, 3, 2, 1},
+                {4, 5, 1, 2, 3},
+                {3, 4, 5, 1, 0}};
+
+
+            int[,] alteredMatrix = Problem8.ZeroMatrix(image);
+
+            for(int i = 0; i < alteredMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < alteredMatrix.GetLength(1); j++)
+                {
+                    Console.Write(alteredMatrix[i, j]);
+                }
+
+                Console.Write("\n");
+            }
         }
     }
 }
+
 
