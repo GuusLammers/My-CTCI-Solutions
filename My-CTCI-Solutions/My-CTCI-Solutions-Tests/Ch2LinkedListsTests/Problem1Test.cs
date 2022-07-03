e3ef730c71f87a75b8176a6d2c238271d0054299
GuusLammers <guuslammers16@gmail.com>
2022-07-03 15:31:52 -0700

    Finished Ch.2 problem 1.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch2LinkedListsTests/Problem1Test.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch2LinkedLists;
+
+namespace My_CTCI_Solutions_Tests.Ch2LinkedListsTests
+{
+    [TestClass]
+    public class Problem1Test
+    {
+        public static bool Equal(Node head1, Node head2)
+        {
+            Node currentNode1 = head1;
+            Node currentNode2 = head2;
+            while(currentNode1.GetNextNode() != null || currentNode2.GetNextNode() != null)
+            {
+                if(!currentNode1.GetValue().Equals(currentNode2.GetValue()))
+                {
+                    return false;
+                }
+
+                currentNode1 = currentNode1.GetNextNode();
+                currentNode2 = currentNode2.GetNextNode();
+            }
+
+            if (currentNode1.GetNextNode() == null && currentNode2.GetNextNode() != null)
+            {
+                return false;
+            }
+            if (currentNode1.GetNextNode() != null && currentNode2.GetNextNode() == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest1()
+        {
+            // arrange
+            Node head = new Node(4);
+            head.AppendToTail(6);
+            head.AppendToTail(3);
+            head.AppendToTail(5);
+            head.AppendToTail(6);
+            head.AppendToTail(1);
+            head.AppendToTail(3);
+            head.AppendToTail(7);
+            head.AppendToTail(4);
+            head.AppendToTail(5);
+            head.AppendToTail(9);
+
+            Node headExpected = new Node(4);
+            headExpected.AppendToTail(6);
+            headExpected.AppendToTail(3);
+            headExpected.AppendToTail(5);
+            headExpected.AppendToTail(1);
+            headExpected.AppendToTail(7);
+            headExpected.AppendToTail(9);
+
+            // act
+            Problem1.RemoveDuplicates(ref head);
+
+            // assert
+            Assert.AreEqual(true, Equal(head, headExpected));
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTest2()
+        {
+            // arrange
+            Node head = new Node(4);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(6);
+            head.AppendToTail(12);
+
+            Node headExpected = new Node(4);
+            headExpected.AppendToTail(6);
+            headExpected.AppendToTail(12);
+
+            // act
+            Problem1.RemoveDuplicates(ref head);
+
+            // assert
+            Assert.AreEqual(true, Equal(head, headExpected));
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch2LinkedLists/Problem1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch2LinkedLists
+{
+    public class Problem1
+    {
+        // Write code to remove duplicates from an unsorted linked list.
+
+        public static void RemoveDuplicates(ref Node head)
+        {
+            Node currentNodePointer = head;
+
+            while (currentNodePointer.GetNextNode() != null)
+            {
+                Node duplicateRemoverPointer = currentNodePointer;
+                while(duplicateRemoverPointer.GetNextNode() != null)
+                {
+                    if(currentNodePointer.GetValue().Equals(duplicateRemoverPointer.GetNextNode().GetValue()))
+                    {
+                        duplicateRemoverPointer.SetNextNode(duplicateRemoverPointer.GetNextNode().GetNextNode());
+                    }
+                    else
+                    {
+                        duplicateRemoverPointer = duplicateRemoverPointer.GetNextNode();
+                    }
+                }
+
+                currentNodePointer = currentNodePointer.GetNextNode();
+            }
+
+        }
+    }
+}
