7455b9e78e7db2920159427be69c64951c33b540
GuusLammers <guuslammers16@gmail.com>
2022-07-03 16:17:42 -0700

    Finished Ch.2 problem 2.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch2LinkedListsTests/Problem2Test.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch2LinkedLists;
+
+namespace My_CTCI_Solutions_Tests.Ch2LinkedListsTests
+{
+    [TestClass]
+    public class Problem2Test
+    {
+        [TestMethod]
+        public void RemoveRemoveKthToLastElementTest1()
+        {
+            // arrange
+            Node head = new Node(4);
+            head.AppendToTail(6);
+            head.AppendToTail(3);
+            head.AppendToTail(5);
+            head.AppendToTail(6);
+            head.AppendToTail(1);
+            head.AppendToTail(3);
+            head.AppendToTail(7);
+            head.AppendToTail(4);
+            head.AppendToTail(5);
+            head.AppendToTail(9);
+
+            Node headExpected = new Node(4);
+            headExpected.AppendToTail(6);
+            headExpected.AppendToTail(3);
+            headExpected.AppendToTail(5);
+            headExpected.AppendToTail(6);
+            headExpected.AppendToTail(1);
+            headExpected.AppendToTail(3);
+            headExpected.AppendToTail(4);
+            headExpected.AppendToTail(5);
+            headExpected.AppendToTail(9);
+
+            int k = 4;
+
+            // act
+            Problem2.RemoveKthToLastElement(ref head, k);
+
+            // assert
+            Assert.AreEqual(true, Node.Equal(head, headExpected));
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch2LinkedLists/Problem2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch2LinkedLists
+{
+    public class Problem2
+    {
+        public static void RemoveKthToLastElement(ref Node head, int k)
+        {
+            // assume first element is element 0
+            Node leadPointer = head;
+            Node followPointer = head;
+
+            // move lead pointer by k elements
+            for(int i = 1; i < k; i++)
+            {
+                if(leadPointer.GetNextNode() == null)
+                {
+                    throw new ArgumentException("k is larger then length of linked list");
+                }
+
+                leadPointer = leadPointer.GetNextNode();
+            }
+
+            // move lead pointer and follow pointer until lead pointer reaches end of linked list
+            while(leadPointer.GetNextNode() != null)
+            {
+                leadPointer = leadPointer.GetNextNode();
+
+                if(leadPointer.GetNextNode() != null)
+                {
+                    followPointer = followPointer.GetNextNode();
+                }
+            }
+
+            // remove kth element from end of the linked list
+            followPointer.SetNextNode(followPointer.GetNextNode().GetNextNode());
+        }
+    }
+}
+
