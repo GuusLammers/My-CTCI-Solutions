312fb8e106cac98efcf83b3d27d5201a133c6309
GuusLammers <guuslammers16@gmail.com>
2022-07-03 09:44:21 -0700

    Finished Ch.1 problem 8.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem8Test.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem8Test
+    {
+        private bool AreNonJaggedArraysEqual(int[,] arr1, int[,] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+
+            if (arr1.GetLength(0) != arr2.GetLength(0) && arr1.GetLength(1) != arr2.GetLength(1))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    if (arr1[i, j] != arr2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void ZeroMatrixTest1Equal()
+        {
+            // arrange
+            int[,] matrix = new int[4, 5] {
+                {1, 2, 3, 4, 5},
+                {5, 0, 3, 2, 1},
+                {4, 5, 1, 2, 3},
+                {3, 4, 5, 1, 0}};
+            int[,] expected = new int[4, 5] {
+                {1, 0, 3, 4, 0},
+                {0, 0, 0, 0, 0},
+                {4, 0, 1, 2, 0},
+                {0, 0, 0, 0, 0}};
+
+            // act
+            int[,] actual = Problem8.ZeroMatrix(matrix);
+
+            // assert
+            Assert.AreEqual(true, AreNonJaggedArraysEqual(actual, expected));
+        }
+
+        [TestMethod]
+        public void ZeroMatrixTest2NotEqual()
+        {
+            // arrange
+            int[,] matrix = new int[4, 5] {
+                {1, 2, 3, 4, 5},
+                {5, 0, 3, 2, 1},
+                {4, 5, 1, 2, 3},
+                {3, 4, 5, 1, 0}};
+            int[,] expected = new int[4, 5] {
+                {1, 0, 3, 4, 0},
+                {0, 0, 0, 0, 0},
+                {4, 0, 1, 2, 0},
+                {3, 0, 5, 1, 0}};
+
+            // act
+            int[,] actual = Problem8.ZeroMatrix(matrix);
+
+            // assert
+            Assert.AreEqual(false, AreNonJaggedArraysEqual(actual, expected));
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem8
+    {
+        // Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+        // column are set to 0.
+
+        public static int[,] ZeroMatrix(int[,] matrix)
+        {
+            int[,] alteredMatrix = (int[,])matrix.Clone();
+
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j] == 0)
+                    {
+                        // overwrite col
+                        for(int z = 0; z < matrix.GetLength(0); z++)
+                        {
+                            alteredMatrix[z, j] = 0;
+                        }
+
+                        // overwrite row
+                        for (int z = 0; z < matrix.GetLength(1); z++)
+                        {
+                            alteredMatrix[i, z] = 0;
+                        }
+                    }
+                }
+            }
+
+            return alteredMatrix;
+        }
+    }
+}
+
