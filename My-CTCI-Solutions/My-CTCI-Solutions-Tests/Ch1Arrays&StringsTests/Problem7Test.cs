616082b899e10f43725125c122cb13287265c990
GuusLammers <guuslammers16@gmail.com>
2022-07-03 09:02:29 -0700

    Finished Ch.1 problem 7.

---
+++ My-CTCI-Solutions/My-CTCI-Solutions-Tests/Ch1Arrays&StringsTests/Problem7Test.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_CTCI_Solutions.Ch1ArraysStrings;
+
+namespace My_CTCI_Solutions_Tests.Ch1ArraysStringsTests
+{
+    [TestClass]
+    public class Problem7Test
+    {
+        private bool AreNonJaggedArraysEqual(int[,] arr1, int[,] arr2)
+        {
+            if(arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+
+            if(arr1.GetLength(0) != arr2.GetLength(0) && arr1.GetLength(1) != arr2.GetLength(1))
+            {
+                return false;
+            }
+
+            for(int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    if(arr1[i, j] != arr2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void RotateMatrix90ClockwiseTest15x5()
+        {
+            // arrange
+            int[,] image = new int[5, 5] {
+                {1, 2, 3, 4, 5},
+                {5, 4, 3, 2, 1},
+                {4, 5, 1, 2, 3},
+                {3, 4, 5, 1, 2},
+                {5, 1, 2, 3, 4}};
+            int[,] expected = new int[5, 5] {
+                {5, 3, 4, 5, 1},
+                {1, 4, 5, 4, 2},
+                {2, 5, 1, 3, 3},
+                {3, 1, 2, 2, 4},
+                {4, 2, 3, 1, 5}}; ;
+
+            // act
+            Problem7.RotateMatrix90Clockwise(ref image);
+
+            // assert
+            Assert.AreEqual(true, AreNonJaggedArraysEqual(image, expected));
+        }
+
+        [TestMethod]
+        public void RotateMatrix90ClockwiseTest24x4()
+        {
+            // arrange
+            int[,] image = new int[4, 4] {
+                {1, 2, 3, 4},
+                {5, 4, 3, 2},
+                {4, 5, 1, 2},
+                {3, 4, 5, 1}};
+            int[,] expected = new int[4, 4] {
+                {3, 4, 5, 1},
+                {4, 5, 4, 2},
+                {5, 1, 3, 3},
+                {1, 2, 2, 4}};
+
+            // act
+            Problem7.RotateMatrix90Clockwise(ref image);
+
+            // assert
+            Assert.AreEqual(true, AreNonJaggedArraysEqual(image, expected));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RotateMatrix90ClockwiseTest3Exception()
+        {
+            // arrange
+            int[,] image = new int[5, 4] {
+                {1, 2, 3, 4},
+                {5, 4, 3, 2},
+                {4, 5, 1, 2},
+                {3, 4, 5, 1},
+                {3, 4, 5, 1}};
+
+            // act
+            Problem7.RotateMatrix90Clockwise(ref image);
+        }
+    }
+}
+
---
+++ My-CTCI-Solutions/My-CTCI-Solutions/Ch1Arrays&Strings/Problem7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace My_CTCI_Solutions.Ch1ArraysStrings
+{
+    public class Problem7
+    {
+        // Given an image represented by an NxN matrix, where each pixel in the image is 4
+        // bytes, write a method to rotate the image by 90 degrees.Can you do this in place?
+
+        public static void RotateMatrix90Clockwise(ref int[,] image)
+        {
+            if (image.GetLength(0) != image.GetLength(1))
+            {
+                throw new ArgumentException("Array must be square.");
+            }
+
+            for (int i = 0; i < image.GetLength(0) / 2; i++)
+            {
+                for (int j = i; j < image.GetLength(0) - i - 1; j++)
+                {
+                    // store top left cell in a temporary variable.
+                    int temporary = image[i, j];
+
+                    // move value from left to top.
+                    image[i, j] = image[image.GetLength(0) - j - 1, i];
+
+                    // move elements from bottom to left.
+                    image[image.GetLength(0) - j - 1, i] = image[image.GetLength(0) - i - 1, image.GetLength(0) - j - 1];
+
+                    // move elements from right to bottom.
+                    image[image.GetLength(0) - i - 1, image.GetLength(0) - j - 1] = image[j, image.GetLength(0) - i - 1];
+
+                    // move elements from top to right.
+                    image[j, image.GetLength(0) - i - 1] = temporary;
+                }
+            }
+        }
+
+
+        ///[1,1,1,1,1
+        /// 2,2,2,2,2
+        /// 3,3,3,3,3
+        /// 4,4,4,4,4
+        /// 5,5,5,5,5]
+
+        ///[5,4,3,2,1
+        /// 5,4,3,2,1
+        /// 5,4,3,2,1
+        /// 5,4,3,2,1
+        /// 5,4,3,2,1]
+    }
+}
+
